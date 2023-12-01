22fe60c2d026f606ce81a757ed6d4b36aa2eba51
AimanFadil <aimanbello2001@gmail.com>
2023-12-01 17:11:01 +0100

    js finito

--- js/script.js
+++ js/script.js
@@ -14,4 +14,21 @@ document.getElementById('bottone').addEventListener('click', () => {
     let numeriAvideo = document.getElementById('numeriAvideo');
     numeriAvideo.textContent = numeriCasuali.join(', ');
 
+    setTimeout(() => {
+        numeriAvideo.textContent = '';
+
+        for (let i = 0; i < 5; i++) {
+            let num = parseInt(prompt('Inserisci i numeri che hai visto in precedenza:'));
+            numeriInseriti.push(num);
+        }
+
+        let RispostaCorretta = 0;
+        for (let i = 0; i < 5; i++) {
+            if (numeriCasuali.includes(numeriInseriti[i])) {
+                RispostaCorretta++;
+            }
+        }
+
+        alert(`Hai indovinato ${RispostaCorretta} numeri`);
+    }, 30000);
 });
