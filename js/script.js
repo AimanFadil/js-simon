b43ea09e4cfe2f82d981d5266f334940d23939b6
AimanFadil <aimanbello2001@gmail.com>
2023-12-01 16:58:37 +0100

    random numbers and html push

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@
 document.getElementById('bottone').addEventListener('click', () => {
     let numeriCasuali = [];
     let numeriInseriti = [];
-    
-  
+
+    while (numeriCasuali.length < 5) {
+        let num = Math.floor(Math.random() * 100) + 1;
+        if (!numeriCasuali.includes(num)) {
+            numeriCasuali.push(num);
+        }
+    }
+
+    let numeriAvideo = document.getElementById('numeriAvideo');
+    numeriAvideo.textContent = numeriCasuali.join(', ');
+
 });
